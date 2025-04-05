537a9b509bd53d4b689eed483d6a21ad3f10c199
mhy1227 <zsmy1227@outlook.com>
2025-04-05 15:41:25 +0800

    feat: 更新登录功能SQL脚本，修改字段名为pwd

--- src/main/resources/db/update/V1__add_login_fields.sql
+++ src/main/resources/db/update/V1__add_login_fields.sql
@@ -1,4 +1,4 @@
 -- 添加登录相关字段
 ALTER TABLE student 
-ADD COLUMN password VARCHAR(100) NOT NULL DEFAULT '123456' COMMENT '登录密码' AFTER sfzh,
-ADD COLUMN login_error_count INT DEFAULT 0 COMMENT '登录错误次数' AFTER password; 
+ADD COLUMN pwd VARCHAR(100) NOT NULL COMMENT '登录密码' AFTER sfzh,
+ADD COLUMN login_error_count INT DEFAULT 0 COMMENT '登录错误次数' AFTER pwd; 
